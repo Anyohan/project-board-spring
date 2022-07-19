d493aeb064e7655e61e768f9d0e8faf34a418c93
yohan <dygks3240@hanyang.ac.kr>
2022-07-19 15:16:33 +0900

    #6 - 스프링부트 프로젝트 시작 스프링부트 프로젝트 초기화 들어간 기술
    * 스프링 웹
    * 엑츄에이터
    * 데브툴스
    * Lombok

---
+++ project-board-spring/src/test/java/com/spring/projectboardspring/ProjectBoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projectboardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
